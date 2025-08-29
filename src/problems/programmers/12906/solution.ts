92ed854f7ab727369121791ed97013aa397f599e
Sam <samankeyc@gmail.com>
2025-08-29 16:58:41 +0900

    task: remove comment

--- src/problems/programmers/12906/solution.ts
+++ src/problems/programmers/12906/solution.ts
@@ -1,12 +1,11 @@
 // programmers 12906 같은-숫자는-싫어
 export function solve(input: string): string {
-  // TODO: implement
-  return "";
+  return '';
 }
 
 // stdin 실행 (BOJ 등)
-import fs from "node:fs";
-if (import.meta.url === "file://" + process.argv[1]) {
-  const data = fs.readFileSync(0, "utf8");
+import fs from 'node:fs';
+if (import.meta.url === 'file://' + process.argv[1]) {
+  const data = fs.readFileSync(0, 'utf8');
   process.stdout.write(solve(data));
 }
